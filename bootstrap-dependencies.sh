2f992cb8914b3a699f5181aa84d69b57136e772b
Doug Black <dblack@twilio.com>
2016-08-26 12:40:01 +0000

    updating bootstrap-dependencies

--- bootstrap-dependencies.sh
+++ bootstrap-dependencies.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
+sudo apt-get update
+
 sudo apt-get -y install git
 sudo apt-get -y install python-pip
 sudo apt-get -y install exuberant-ctags
 sudo apt-get -y install zsh
 sudo apt-get -y install silversearcher-ag
 
-sudo pip install --user powerline-status
-sudo pip install --user virtualenvwrapper
+sudo -u `whoami` pip install --user "powerline-status"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sudo apt-get -y autoremove
